b7ce03f46094bd6eaef12131957100e0956d7ab6
iridiumJester <potatofishum@gmail.com>
2025-11-07 11:52:15 -0800

    Wrote pseudocode for main process

--- src/inventory.py
+++ src/inventory.py
@@ -3,24 +3,32 @@ inventory.py
 by iridiumJester
 The inventory where items are stored and edited.
 """
-
 from item import Item 
 
 class Inventory():
     def __init__(self):    
+        # Dictionary for items
         self.items = {}
 
     def add_item(self, item: Item):
         if item.name in self.items.keys():
-            current_number = self.items.get(item.name)
-            self.items[item.name] = item.count + current_number
+            # current_number = self.items.get(item.name)
+            # self.items[item.name] = item.count + current_number
+            """
+            if current item name is equal to another item
+                add current item count to said item
+                delete current item
+            """
         else:
             self.items[item.name] = item.count
 
 
 if __name__ == "__main__":
-    apples = Item("Apples", 4)
+    # Create category to create items
     pantry = Inventory()
+
+    # Create items and add to inventory
+    apples = Item("Apples", 4)
     pantry.add_item(apples)
     apples = Item("Apples", 8)
     pantry.add_item(apples)
--- src/kitsch_inventory.py
+++ src/kitsch_inventory.py
@@ -6,6 +6,40 @@ An organizational/tracking tool for your kitchen.
 
 def main():
     print("Welcome to Kitsch-Inventory.")
+    """
+    DISPLAY additional instructions
+    DISPLAY start a new file or begin import. [ new/import ]
+    IF option 1
+        SET inventory as an empty list
+    ELSE
+        idk how to do that yet so ignore for now
+    SET option to 0
+    WHILE option != 4
+        DISPLAY what would you like to do? [ add/edit/view/save and exit ]
+        GET option from user
+        IF option 1
+            IF no categories
+                GET category name from user
+            ELSE
+                DISPLAY categories
+                GET category from user by its number in the list
+            add item
+        ELSE IF option 2
+            DISPLAY [ item/category ]
+            GET user input
+            then you give them a kabillion options
+        ELSE IF option 3
+            DISPLAY inventory
+            this would print a little at a time so it would need user input for movement between pages
+        ELSE IF option 4
+            DISPLAY saving and exiting...
+        ELSE
+            DISPLAY not an option
+        END IF
+    END WHILE
+    export data to a json or smtn somehow
+    DISPLAY bye-bye!
+    """
 
 
 if __name__ == "__main__":
